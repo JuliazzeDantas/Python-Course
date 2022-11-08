6c4accd73599e42f2f05deea3e0b86f21c8908b8
Vinícius Juliasse Dantas <55809294+JuliazzeDantas@users.noreply.github.com>
2022-11-08 09:27:23 -0300

    Update ex020.py

--- COURSE_1/ex020.py
+++ COURSE_1/ex020.py
@@ -1,1 +1,17 @@
-'''Exercise: '''
+'''Exercise: Write a program to read the name of four people and to print all names in a random order'''
+
+import random
+from random import shuffle
+from random import choice
+
+name1 = input("Enter the first name: ")
+name2 = input("Enter the first name: ")
+name3 = input("Enter the first name: ")
+name4 = input("Enter the first name: ")
+
+list = [name1, name2, name3, name4]
+
+for i in range(0,4): ###########My method
+    randomName = random.choice(list)
+    print(randomName)
+    list.remove(randomName)
